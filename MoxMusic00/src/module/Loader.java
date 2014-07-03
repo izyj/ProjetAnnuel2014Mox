662d12e8222557def332f23a5bca50d07c934c26
izyj <iisiramen@gmail.com>
2014-07-04 00:12:56 +0200

    IIS Plugin modif

--- MoxMusic00/src/module/Loader.java
+++ MoxMusic00/src/module/Loader.java
@@ -1,109 +1,78 @@
-package module;
-
-import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
-
-public class Loader {
-	private static List<URL> urls = new ArrayList<URL>();
-
-	
-	private static List<String> getModuleClasses(){ 
-		List<String> classes = new ArrayList<String>(); 
- 
-		//On liste les fichiers de module  
-		File[] files = new File("dossier").listFiles(new ModuleFilter()); 
- 
-		for(File f : files){ 
-			JarFile jarFile = null; 
- 
-			try { 
-				//On ouvre le fichier JAR 
-				jarFile = new JarFile(f); 
-				
-				
-				//On récupère le manifest 
-				Manifest manifest = jarFile.getManifest(); 
- 
-				//On récupère la classe 
-				String moduleClassName = manifest.getMainAttributes().getValue("Module-Class"); 
- 
-				classes.add(moduleClassName); 
- 
-				urls.add(f.toURI().toURL()); 
-			} catch (IOException e) { 
-				e.printStackTrace(); 
-			} finally { 
-				if(jarFile != null){ 
-					try { 
-						jarFile.close(); 
-					} catch (IOException e) { 
-						e.printStackTrace(); 
-					} 
-				} 
-			} 
-		} 
- 
-		return classes; 
-	}
-	
-	private static class ModuleFilter implements FileFilter { 
-		@Override 
-		public boolean accept(File file) { 
-			return file.isFile() && file.getName().toLowerCase().endsWith(".jar"); 
-		} 
-	}
-	
-	private static ClassLoader classLoader; 
-	 
-	public static List<IModule> loadModules(){ 
-		List<IModule> modules = new ArrayList<IModule>(); 
-	 
-		List<String> classes = getModuleClasses(); 
-	 
-		AccessController.doPrivileged(new PrivilegedAction<Object>(){ 
-			@Override 
-			public Object run() { 
-				classLoader = new URLClassLoader( 
-				urls.toArray(new URL[urls.size()]),Loader.class.getClassLoader()); 
-				
-				return null; 
-			} 
-		}); 
-	 
-		for(String c : classes){ 
-			try { 
-				Class<?> moduleClass = Class.forName(c, true, classLoader); 
-	 
-				if(IModule.class.isAssignableFrom(moduleClass)){ 
-					Class<IModule> castedClass = (Class<IModule>)moduleClass; 
-	 
-					IModule module = castedClass.newInstance(); 
-	 
-					modules.add(module); 
-				}  
-			} catch (ClassNotFoundException e1) { 
-				e1.printStackTrace(); 
-			} catch (InstantiationException e) { 
-				e.printStackTrace(); 
-			} catch (IllegalAccessException e) { 
-				e.printStackTrace(); 
-			} 
-		} 
-	 
-		return modules; 
-	}
-
-	
-		
-		
-}
-
+//package module;
+//
+//import java.io.File;
+//import java.net.URL;
+//import java.net.URLClassLoader;
+//import java.util.Enumeration;
+//import java.util.jar.JarFile;
+//
+//
+//public class Loader {
+//
+//	
+//private File files = new File(".\\src\\plugin\\Jar");
+//
+//public Loader() {
+//	
+//	  
+//			File[] f = files.listFiles();
+////			Pour charger le .jar en memoire
+//			URLClassLoader loader;
+//			//Pour la comparaison de chaines
+//			String tmp = "";
+//			//Pour le contenu de l'archive jar
+//			Enumeration enumeration;
+//			//Pour déterminé quels sont les interfaces implémentées
+//			Class tmpClass = null;
+//	 
+//			for(int index = 0 ; index < f.length ; index ++ ){
+//				f[index] = new File(files[index]);
+//				if( !f[index].exists() ) {
+//					break;
+//				}
+//	 
+//				URL u = f[index].toURL();
+//				//On créer un nouveau URLClassLoader pour charger le jar qui se trouve ne dehors du CLASSPATH
+//				loader = new URLClassLoader(new URL[] {u}); 
+//	 
+//				//On charge le jar en mémoire
+//				JarFile jar = new JarFile(f[index].getAbsolutePath());
+//	 
+//				//On récupére le contenu du jar
+//				enumeration = jar.entries();
+//	 
+//				while(enumeration.hasMoreElements()){
+//	 
+//					tmp = enumeration.nextElement().toString();
+//	 
+//					//On vérifie que le fichier courant est un .class (et pas un fichier d'informations du jar )
+//					if(tmp.length() > 6 && tmp.substring(tmp.length()-6).compareTo(".class") == 0) {
+//	 
+//						tmp = tmp.substring(0,tmp.length()-6);
+//						tmp = tmp.replaceAll("/",".");
+//	 
+//						tmpClass = Class.forName(tmp ,true,loader);
+//	 
+//						for(int i = 0 ; i < tmpClass.getInterfaces().length; i ++ ){
+//	 
+//							//Une classe ne doit pas appartenir à deux catégories de plugins différents. 
+//							//Si tel est le cas on ne la place que dans la catégorie de la première interface correct
+//							// trouvée
+//							if(tmpClass.getInterfaces()[i].toString().equals("tutoPlugins.plugins.StringPlugins") ) {
+//								this.classStringPlugins.add(tmpClass);
+//							}
+//							else {
+//								if( tmpClass.getInterfaces()[i].toString().equals("tutoPlugins.plugins.IntPlugins") ) {
+//									this.classIntPlugins.add(tmpClass);
+//								}
+//							}
+//						}
+//	 
+//					}
+//				}
+//	 
+//	 
+//			}
+// }
+//		
+//}
--- MoxMusic00/src/module/ModuleLoader.java
+++ MoxMusic00/src/module/ModuleLoader.java
@@ -1,103 +1,128 @@
 package module;
 
+
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.util.jar.Manifest;
+import java.util.jar.JarFile;
+import java.util.ArrayList;
+import java.util.List;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
-
-public class ModuleLoader { 
-	private static List<URL> urls = new ArrayList<URL>(); 
  
-	private static List<String> getModuleClasses(){ 
-		List<String> classes = new ArrayList<String>(); 
+/**
+ * Classe responsable du chargement des modules.
+ *
+ * @author Baptiste Wicht
+ */
+public class ModuleLoader {
+    private static List<URL> urls = new ArrayList<URL>();
  
-		//On liste les fichiers de module  
-		File[] files = new File("dossier").listFiles(new ModuleFilter()); 
+    private static ClassLoader classLoader;
  
-		for(File f : files){ 
-			JarFile jarFile = null; 
+    /**
+     * Charge tous les modules et les retourne.
+     *
+     * @return Une List contenant tous les modules préalablement chargés.
+     */
+    public static List<IModule> loadModules() {
+        List<IModule> modules = new ArrayList<IModule>();
  
-			try { 
-				//On ouvre le fichier JAR 
-				jarFile = new JarFile(f); 
+        List<String> classes = getModuleClasses();
  
-				//On r�cup�re le manifest 
-				Manifest manifest = jarFile.getManifest(); 
+        AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            @Override
+            public Object run() {
+                classLoader = new URLClassLoader(
+                        urls.toArray(new URL[urls.size()]),
+                        ModuleLoader.class.getClassLoader());
  
-				//On r�cup�re la classe 
-				String moduleClassName = manifest.getMainAttributes().getValue("Module-Class"); 
+                return null;
+            }
+        });
  
-				classes.add(moduleClassName); 
+        for (String c : classes) {
+            try {
+                Class<?> moduleClass = Class.forName(c, true, classLoader);
  
-				urls.add(f.toURI().toURL()); 
-			} catch (IOException e) { 
-				e.printStackTrace(); 
-			} finally { 
-				if(jarFile != null){ 
-					try { 
-						jarFile.close(); 
-					} catch (IOException e) { 
-						e.printStackTrace(); 
-					} 
-				} 
-			} 
-		} 
+                if (IModule.class.isAssignableFrom(moduleClass)) {
+                    Class<IModule> castedClass = (Class<IModule>) moduleClass;
  
-		return classes; 
-	} 
+                    IModule module = castedClass.newInstance();
  
-	private static class ModuleFilter implements FileFilter { 
-		@Override 
-		public boolean accept(File file) { 
-			return file.isFile() && file.getName().toLowerCase().endsWith(".jar"); 
-		} 
+                    modules.add(module);
+                }
+            } catch (ClassNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+        return modules;
+    }
+ 
+    /**
+     * Retourne toutes les classes de modules à charger.
+     *
+     * @return Une List contenant les noms des classes à instancier.
+     */
+    private static List<String> getModuleClasses() {
+        List<String> classes = new ArrayList<String>();
+ 
+        //On liste les fichiers de module
+        File[] files = new File(Core.getInstance().getApplicationFolder() + "/modules/").listFiles(new ModuleFilter());
+ 
+        for (File f : files) {
+            JarFile jarFile = null;
+ 
+            try {
+                //On ouvre le fichier JAR
+                jarFile = new JarFile(f);
+ 
+                //On récupère le manifest
+                Manifest manifest = jarFile.getManifest();
+ 
+                //On récupère la classe
+                String moduleClassName = manifest.getMainAttributes().getValue("Module-Class");
+ 
+                classes.add(moduleClassName);
+ 
+                urls.add(f.toURI().toURL());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (jarFile != null) {
+                    try {
+                        jarFile.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+ 
+        return classes;
+    }
+ 
+    /**
+     * Un filtre de fichiers pour les modules. Ce filtre ne prend en compte que les fichiers .jar.
+     */
+    private static class ModuleFilter implements FileFilter {
+        @Override
+        public boolean accept(File file) {
+            return file.isFile() && file.getName().toLowerCase().endsWith(".jar");
+		}
 	}
-	private static ClassLoader classLoader; 
+}
 
-	public static List<IModule> loadModules(){ 
-		List<IModule> modules = new ArrayList<IModule>(); 
-	 
-		List<String> classes = getModuleClasses(); 
-	 
-		AccessController.doPrivileged(new PrivilegedAction<Object>(){ 
-			@Override 
-			public Object run() { 
-				classLoader = new URLClassLoader( 
-				urls.toArray(new URL[urls.size()]),  
-				ModuleLoader.class.getClassLoader()); 
-	 
-				return null; 
-			} 
-		}); 
-	 
-		for(String c : classes){ 
-			try { 
-				Class<?> moduleClass = Class.forName(c, true, classLoader); 
-	 
-				if(IModule.class.isAssignableFrom(moduleClass)){ 
-					Class<IModule> castedClass = (Class<IModule>)moduleClass; 
-	 
-					IModule module = castedClass.newInstance(); 
-	 
-					modules.add(module); 
-				}  
-			} catch (ClassNotFoundException e1) { 
-				e1.printStackTrace(); 
-			} catch (InstantiationException e) { 
-				e.printStackTrace(); 
-			} catch (IllegalAccessException e) { 
-				e.printStackTrace(); 
-			} 
-		} 
-	 
-		return modules; 
-	}
-} 
+	
+		
+		
+
 
